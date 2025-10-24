a222b1ce400f985514e3739f0b6bf2dd95a5c9ae
sadghoblin <171681941+SADGHOBLIN@users.noreply.github.com>
2025-10-24 15:54:26 +0100

    Create function to POST form data to API and log result to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@ const API_KEY = "QGJf7Nj8sMO42SSKxpdf9bYy1x8";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
-// button to check is API key is valid
-document.getElementById("status").addEventListener("click", e => getStatus(e)); 
+// Add event listeners to buttons
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+// GET request to check if API key is valid
 async function getStatus(e) {
     const queryString =`${API_URL}?api_key=${API_KEY}`;
     const response = await fetch(queryString);
@@ -26,4 +28,23 @@ function displayStatus(data) {
     document.getElementById("results-content").innerHTML = results;
 
     resultsModal.show();
+}
+
+// POST request to send JS code to be tested, log results to console
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+    const response = await fetch(API_URL, {
+                                method: "POST",
+                                headers: {
+                                    "Authorization": API_KEY,
+                                    },
+                                    body: form,
+    });
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    } else {
+        throw new Error(data.error);
+    }
 }
